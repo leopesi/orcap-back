573244e05b6571938c7bdceb9002c2bf5bdf1417
Lucas Torres <senomar_detritos@yahoo.com.br>
2021-09-15 13:43:27 -0300

    init default data do lojista

---
+++ src/auto/brands.js
@@ -0,0 +1,14 @@
+module.exports = {
+    fields:[
+        'id', 'name', 'cnpj', 'cidade', 'bairro', 'cep', 'endereço', 'número', 'e-mail', 'telefone', 'website'
+    ],
+    data: [        
+        ['Sodramar', '51.333.797/0003-41', 'Diadema ', 'Vila Conceição', '09911-510', 'Caramuru', '925', 'marketing@sodramar.com.br', '', 'www.sodramar.com.br'],
+        ['Viniplas ', '04.543.319/0001-14', 'Atibaia ', 'Boa Vista', '12954-103', 'Estr. dos Perines', '485 ', 'viniplas@viniplas.com.br', '(11) 4411-9662', 'www.viniplas.com.br'],
+        ['Nautilus', '53.476.057/0001-28', 'Nazaré Paulista', 'Tanque Preto', '12960-000', 'Estr. Mun. Pref. Geraldo Ramos Gonçalves', '236', '', '(11) 4597-7222', 'www.nautilus.ind.br'],
+        ['Jacuzzi', '59.105.007/0001-10', 'Itu', 'Melissa', '13308-900', 'Rod. Waldomiro C. Camargo', 'km 53,5 - SP-79', '', '11 3164-7632', 'www.jacuzzi.com.br'],
+        ['Dancor', '33.561.853/0001-51', 'São Paulo', 'Morumbi', '04703-003', 'Av. Morumbi', '7867 ', 'vendas@dancor.com.br', '(11) 5044-2019', 'www.dancor.com.br'],
+        ['Mineração e Moagem São João Batista', '55.661.748/0001-27', 'Queluz ', 'FOGUETEIRO', '12800-000', 'Estrada Municipal do Fogueteiro', 's/n', 'contato@mineracaosaojoao.com', '(12) 3147-9090', 'www.mineracaosaojoao.com'],
+        ['Sibrape', '57.716.854/0001-96', 'Ribeirão Preto', 'Pq. Industrial Lagoinha', '', 'Rua Armando Tarozzo', '210', 'sac@sibrape.com', '(16) 2101-7000', 'www.sibrape.com.br'],
+    ]
+}
